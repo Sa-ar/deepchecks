7748579f9a9f47fe8bf30976867a79e691b8cff0
Saar Davidson <davidsonsaar@gmail.com>
2022-12-31 15:29:08 +0200

    add csv parsing hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { useParse } from "./hooks/useParse";
+
 import './App.css';
 
 function App() {
+  const { isLoading, columns, errors, isErrors, headers } = useParse("/house_prices.csv");
+
   return (
     <div className="App">
+      {isErrors && (
+        <div>
+          <>Error: {JSON.stringify(errors)}</>
+        </div>
+      )}
+      {!isErrors && isLoading && "Loading..."}
+      <pre style={{ width: "100%", textAlign: "left" }}>
+        {JSON.stringify(headers)}
+        {!isErrors && !isLoading && JSON.stringify(columns, null, 2)}
+      </pre>
     </div>
   );
 }
---
+++ src/hooks/useParse.ts
@@ -0,0 +1,67 @@
+import { parse, ParseError, ParseMeta, ParseWorkerConfig } from "papaparse";
+import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+
+export function useParse(url: string) {
+  const [columns, setColumns] = useState<unknown[]>([]);
+  const [rows, setRows] = useState<unknown[]>([])
+  const [headers, setHeaders] = useState<string[]>([])
+  const [errors, setErrors] = useState<Array<ParseError | unknown>>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [meta, setMeta] = useState<ParseMeta>();
+
+  const {
+    isLoading: isFileLoading,
+    isError,
+    data: fileData,
+    error,
+  } = useQuery("getCsv", async () => {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    return await response.text();
+  });
+
+
+  useEffect(() => {
+    const parseConfig: ParseWorkerConfig = {
+      dynamicTyping: true,
+      header: true,
+      worker: true,
+      skipEmptyLines: true,
+      complete: function ({ data, errors, meta }) {
+        setHeaders(Object.keys(data[0]))
+        setErrors(errors);
+        setMeta(meta);
+        setRows(data);
+        setColumns(data.reduce((acc, row) => {
+          Object.entries(row).map(([column, value]) => {
+            acc[column] ? acc[column].push(value) : acc[column] = [value]
+          });
+          return acc;
+        }, {}));
+        setIsLoading(false);
+      }
+    };
+
+    if (!isFileLoading && !isError && typeof fileData === "string") {
+      parse(fileData, parseConfig);
+    } else if (isFileLoading) {
+      setIsLoading(true);
+    } else if (isError) {
+      setErrors([error]);
+    }
+  }, [fileData, isFileLoading, isError, error])
+
+  return {
+    columns,
+    rows,
+    errors,
+    headers,
+    isErrors: !!errors.length,
+    isLoading,
+    meta
+  };
+}
